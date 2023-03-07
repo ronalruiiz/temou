72299849b0602b125733f6562834ee443a33045e
ronalruiiz <ronalruizdev@gmail.com>
2023-03-06 22:02:05 -0500

    fixed: graphics

--- src/charts/RadarCharts.js
+++ src/charts/RadarCharts.js
@@ -1,44 +1,44 @@
 import React, { PureComponent } from 'react';
 import { Radar, RadarChart, PolarGrid, Legend, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
 
-const data = [
-  {
-    subject: 'Math',
-    A: 120,
-    B: 110,
-    fullMark: 150,
-  },
-  {
-    subject: 'Chinese',
-    A: 98,
-    B: 130,
-    fullMark: 150,
-  },
-  {
-    subject: 'English',
-    A: 86,
-    B: 130,
-    fullMark: 150,
-  },
-  {
-    subject: 'Geography',
-    A: 99,
-    B: 100,
-    fullMark: 150,
-  },
-  {
-    subject: 'Physics',
-    A: 85,
-    B: 90,
-    fullMark: 150,
-  },
-  {
-    subject: 'History',
-    A: 65,
-    B: 85,
-    fullMark: 150,
-  },
-];
+// const data = [
+//   {
+//     subject: 'Math',
+//     A: 120,
+//     B: 110,
+//     fullMark: 150,
+//   },
+//   {
+//     subject: 'Chinese',
+//     A: 98,
+//     B: 130,
+//     fullMark: 150,
+//   },
+//   {
+//     subject: 'English',
+//     A: 86,
+//     B: 130,
+//     fullMark: 150,
+//   },
+//   {
+//     subject: 'Geography',
+//     A: 99,
+//     B: 100,
+//     fullMark: 150,
+//   },
+//   {
+//     subject: 'Physics',
+//     A: 85,
+//     B: 90,
+//     fullMark: 150,
+//   },
+//   {
+//     subject: 'History',
+//     A: 65,
+//     B: 85,
+//     fullMark: 150,
+//   },
+// ];
 
 export default function RadarCharts({exams}){
   let data = []
@@ -57,7 +57,7 @@ export default function RadarCharts({exams}){
     })
     
     let result =  (responses / questions.length ) *100
-    data.push({"subject":exam.therapy.name,"A":responses,"B":questions.length,"fullMark":parseInt(result)})
+    data.push({"subject":exam.therapy.name,"A":responses,"B":questions.length})
   });
   
     return (
@@ -65,8 +65,9 @@ export default function RadarCharts({exams}){
         <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
           <PolarGrid />
           <PolarAngleAxis dataKey="subject" />
-          <PolarRadiusAxis angle={30} domain={[0, 150]} />
-          
+          <PolarRadiusAxis  />
+          <Radar name="Respondidas" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+          <Radar name="Preguntas" dataKey="B" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.6} />
           <Legend />
         </RadarChart>
       </ResponsiveContainer>
